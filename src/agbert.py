f75576fc502970b3a8bbd66851b965ee8e3bdef4
yonde927 <svyuta1202@gmail.com>
2022-03-14 00:51:38 +0900

    launch

--- src/agbert.py
+++ src/agbert.py
@@ -11,18 +11,23 @@ import time
 import pickle
 import os
 
+
 class Parser:
     """
     意味類似用の検索器ベースオブジェクト
     """
+
     def __init__(self):
-        self.matrix=[]
-        self.docs=[]
-    def register(self,docs :list):
+        self.matrix = []
+        self.docs = []
+
+    def register(self, docs: list):
         pass
-    def similardoc(self,text :str):
+
+    def similardoc(self, text: str):
         pass
-    def save(self,path="./data/matrix.pkl"):
+
+    def save(self, path="./data/matrix.pkl"):
         """
         登録文書と特徴量を保存
 
@@ -31,29 +36,33 @@ class Parser:
         path : str
             保存先
         """
-        os.makedirs(os.path.dirname(path),exist_ok=True)
-        with open(path,"wb") as f:
-            pickle.dump((self.docs,self.matrix),f)
-    def load(self,path="./data/matrix.pkl"):
-        os.makedirs(os.path.dirname(path),exist_ok=True)
-        with open(path,"rb") as f:
-            self.docs,self.matrix = pickle.load(path)
-class EnBertEmb(Parser):
-        """
-        登録文書と特徴量を読み込み
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "wb") as f:
+            pickle.dump((self.docs, self.matrix), f)
 
+    def load(self, path="./data/matrix.pkl"):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "rb") as f:
+            self.docs, self.matrix = pickle.load(path)
+
+
+class EnBertEmb(Parser):
+    """
+    登録文書と特徴量を読み込み
         Parameters
         ----------
         path : str
             保存先
-        """
+    """
+
     def __init__(self):
         super().__init__()
         if torch.cuda.is_available():
-            self.model= SentenceTransformer('all-MiniLM-L6-v2',device='cuda')
+            self.model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
         else:
-            self.model= SentenceTransformer('all-MiniLM-L6-v2')
-    def register(self,docs):
+            self.model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    def register(self, docs):
         """
         検索器にドキュメントを登録し、エンコード＆保持
 
@@ -72,7 +81,8 @@ class EnBertEmb(Parser):
             output = torch.Tensor(self.model.encode([d]))
             self.matrix.extend(output.to('cpu').tolist())
         return self.matrix
-    def batch_register(self,docs,b=15):
+
+    def batch_register(self, docs, b=15):
         """
         検索器にドキュメントを登録し、エンコード＆保持
         バッチ処理で高速化できる。
@@ -90,17 +100,19 @@ class EnBertEmb(Parser):
         """
         self.matrix = []
         self.docs = docs
-        size=len(docs) // b
-        for i in tqdm(range(size+1)):
+        size = len(docs) // b
+        for i in tqdm(range(size + 1)):
             if i == size:
-                batch = docs[i*b:]
+                batch = docs[i * b:]
             else:
-                batch = docs[i*b:i*b+b]
+                batch = docs[i * b:i * b + b]
             output = torch.Tensor(self.model.encode(batch))
             self.matrix.extend(output.to('cpu').tolist())
-    def encode(self,endoc):
+
+    def encode(self, endoc):
         return torch.Tensor(self.model.encode([endoc]))
-    def similardoc(self,doc,size=-1):
+
+    def similardoc(self, doc, size=-1):
         """
         登録されているコーパスから類似度の高い文章を降順で返す。
 
@@ -118,20 +130,25 @@ class EnBertEmb(Parser):
         start = time.time()
         vector = self.encode(doc)
         corpas = torch.Tensor(self.matrix)
-        if(len(self.matrix)>0):
-            similarity = cosine_similarity(vector,corpas)
+        if(len(self.matrix) > 0):
+            similarity = cosine_similarity(vector, corpas)
             ranking = similarity.squeeze().argsort()[::-1]
-            print("%f sec for search"%(time.time()-start))
-            if(size<0):
+            print("%f sec for search" % (time.time() - start))
+            if(size < 0):
                 size = len(self.docs)
             return [self.docs[i] for i in ranking[:size]]
         else:
             print("No docs are registerd.")
 
+
 class JaBertEmb(Parser):
-    def __init__(self, model_name_or_path = "sonoisa/sentence-bert-base-ja-mean-tokens-v2", device=None):
+    def __init__(
+            self,
+            model_name_or_path="sonoisa/sentence-bert-base-ja-mean-tokens-v2",
+            device=None):
         super().__init__()
-        self.tokenizer = BertJapaneseTokenizer.from_pretrained(model_name_or_path)
+        self.tokenizer = BertJapaneseTokenizer.from_pretrained(
+            model_name_or_path)
         self.model = BertModel.from_pretrained(model_name_or_path)
         self.model.eval()
         if device is None:
@@ -140,9 +157,12 @@ class JaBertEmb(Parser):
         self.model.to(device)
 
     def _mean_pooling(self, model_output, attention_mask):
-        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        # First element of model_output contains all token embeddings
+        token_embeddings = model_output[0]
+        input_mask_expanded = attention_mask.unsqueeze(
+            -1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / \
+            torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     @torch.no_grad()
     def register(self, sentences, batch_size=1):
@@ -166,17 +186,21 @@ class JaBertEmb(Parser):
         for batch_idx in tqdm(iterator):
             batch = sentences[batch_idx:batch_idx + batch_size]
 
-            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest", 
-                                           truncation=True, return_tensors="pt").to(self.device)
+            encoded_input = self.tokenizer.batch_encode_plus(
+                batch,
+                padding="longest",
+                truncation=True,
+                return_tensors="pt").to(
+                self.device)
             model_output = self.model(**encoded_input)
-            sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"]).to('cpu')
+            sentence_embeddings = self._mean_pooling(
+                model_output, encoded_input["attention_mask"]).to('cpu')
 
             all_embeddings.extend(sentence_embeddings)
-        
         self.matrix = torch.stack(all_embeddings)
         # return torch.stack(all_embeddings).numpy()
         return self.matrix
-    
+
     @torch.no_grad()
     def encode(self, sentences, batch_size=1):
         all_embeddings = []
@@ -184,16 +208,21 @@ class JaBertEmb(Parser):
         for batch_idx in tqdm(iterator):
             batch = sentences[batch_idx:batch_idx + batch_size]
 
-            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest", 
-                                           truncation=True, return_tensors="pt").to(self.device)
+            encoded_input = self.tokenizer.batch_encode_plus(
+                batch,
+                padding="longest",
+                truncation=True,
+                return_tensors="pt").to(
+                self.device)
             model_output = self.model(**encoded_input)
-            sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"]).to('cpu')
+            sentence_embeddings = self._mean_pooling(
+                model_output, encoded_input["attention_mask"]).to('cpu')
 
             all_embeddings.extend(sentence_embeddings)
         # return torch.stack(all_embeddings).numpy()
-        return  torch.stack(all_embeddings)
-    
-    def similardoc(self,doc,size=-1):
+        return torch.stack(all_embeddings)
+
+    def similardoc(self, doc, size=-1):
         """
         登録されているコーパスから類似度の高い文章を降順で返す。
 
@@ -211,15 +240,12 @@ class JaBertEmb(Parser):
         start = time.time()
         vector = self.encode([doc])
         corpas = self.matrix
-        if(len(self.matrix)>0):
-            similarity = cosine_similarity(vector,corpas)
+        if(len(self.matrix) > 0):
+            similarity = cosine_similarity(vector, corpas)
             ranking = similarity.squeeze().argsort()[::-1]
-            print("%f sec for search"%(time.time()-start))
-            if(size<0):
+            print("%f sec for search" % (time.time() - start))
+            if(size < 0):
                 size = len(self.docs)
             return [self.docs[i] for i in ranking[:size]]
         else:
             print("No docs are registerd.")
-    
-
-
